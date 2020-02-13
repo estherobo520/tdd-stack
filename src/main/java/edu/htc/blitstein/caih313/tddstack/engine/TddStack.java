edb88f49b958a14c22a7d3406e52a1d1f21d7f94
estherobo520 <55057146+estherobo520@users.noreply.github.com>
2020-02-12 21:49:01 -0600

    Added test file and methods to TddStack

--- src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
+++ src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
@@ -1,4 +1,26 @@
 package edu.htc.blitstein.caih313.tddstack.engine;
 
+import edu.htc.blitstein.caih313.tddstack.IStackable;
+
 public class TddStack {
+    public static long DEFAULT_DEPTH = 100;
+    public long stackDepth;
+    IStackable stackable;
+    public long tddStack = 3;
+
+    public TddStack() {}
+    public TddStack(long stackDepth) {}
+
+    public boolean isEmpty() {
+        return false;
+    }
+    public boolean isFull() {
+        return false;
+    }
+
+    public void push(IStackable stackable) {}
+    public IStackable pop() {
+        return null;
+    }
 }
+
--- src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
+++ src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
@@ -1,9 +1,19 @@
 package edu.htc.blitstein.caih313.tddstack.engine;
 
+import edu.htc.blitstein.caih313.tddstack.IStackable;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class TddStackTest {
+
+    TddStack tddStack = new TddStack();
+    IStackable stackableOne;
+    IStackable stackableTwo;
+    IStackable stackableThree;
+
 
     @Before
     public void setUp() throws Exception {
@@ -11,5 +21,51 @@ public class TddStackTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void TddStack(long stackDepth) {
+        stackDepth = TddStack.DEFAULT_DEPTH;
+        assertEquals(stackDepth, stackDepth);
+    }
+
+    //check that calling isEmpty() on a newly created stack returns "true"
+    @Test
+    public void isEmptyTest() {
+        //return true;
+        assertEquals(true, true);
+
+    }
+
+    //check that calling isFull() on a newly created stack returns "false"
+    @Test
+    public void isFullTest() {
+        //return false;
+        assertEquals(false, false);
+    }
+
+    //check that calling pop() on a newly created stack returns "null"
+    @Test
+    public void popTest() {
+        //return null;
+        assertEquals(null, null);
+    }
+
+    //check that when a IStackable is pushed on the TddStack, then isEmpty() returns false
+    @Test
+    public void isEmptyTestTwo(TddStack tddStack) {
+        tddStack.push(stackableOne);
+        //return false;
+        assertEquals(false, true);
+    }
+
+    //Instantiate a TddStack with a depth of 3. Implement a test that checks that isFull() returns true when three IStackable objects are pushed onto it
+    @Test
+    public void isFullTestTwo(TddStack tddStack) {
+        tddStack.push(stackableOne);
+        tddStack.push(stackableTwo);
+        tddStack.push(stackableThree);
+        //return true;
+        assertEquals(true, false);
     }
 }
